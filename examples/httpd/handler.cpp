c1479ee89cdceaaed4a2eaef275206f67faf33c5
shankusu <shankusu2017@gmail.com>
2024-05-15 15:47:18 +0800

    little

--- examples/httpd/handler.cpp
+++ examples/httpd/handler.cpp
@@ -160,6 +160,7 @@ int Handler::grpc(HttpRequest* req, HttpResponse* resp) {
 }
 
 int Handler::test(const HttpContextPtr& ctx) {
+	usleep(1000000);
 	hlogi("0x03c5c9ed process_id:%ld, pthread_id:%ld\n", hv_getpid(), (long)hv_gettid());
 	
     ctx->setContentType(ctx->type());
